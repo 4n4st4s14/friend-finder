6151da8cf6d6607b1c128f6db19730caf61fcbe0
Anastasia Lucas <ani@theta.local>
2017-11-19 12:37:36 -0500

    user data pushing to api route

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,10 @@
-// data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+// data inside of app/data/friends.js as an array of objects.
+var friendArr = [
+    {
+      "name": "Mildred Bonk",
+      "photo": "http://1.bp.blogspot.com/-vDuS1bdQkf8/TVi1J3kdV6I/AAAAAAAAB0o/jCpnXNjQcFw/s1600/dfwgromit.jpg",
+      "scores": [5,2,5,3,4,1,5,1,2,5],
+    },
+]
+
+module.exports = friendArr;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,14 @@
-//A GET Route to /survey which should display the survey page.
+var path = require('path');
 
+//A GET Route to /survey which should display the survey page.
+module.exports = function(app){
+  app.get('/survey', function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
 
 //A default, catch-all route that leads to home.html which displays the home page.
+  app.use(function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+
+}
