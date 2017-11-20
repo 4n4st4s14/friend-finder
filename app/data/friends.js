f489d430e473393d5631e206c9ebddb1fa01766b
Anastasia Lucas <ani@theta.local>
2017-11-19 22:21:57 -0500

    background

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,42 @@
 // data inside of app/data/friends.js as an array of objects.
 var friendArr = [
     {
-      "name": "Mildred Bonk",
-      "photo": "http://1.bp.blogspot.com/-vDuS1bdQkf8/TVi1J3kdV6I/AAAAAAAAB0o/jCpnXNjQcFw/s1600/dfwgromit.jpg",
-      "scores": [5,2,5,3,4,1,5,1,2,5],
+      "name": "Bill Watterson",
+      "photo": "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/6/9/1402315503914/Bill-Watterson-010.jpg?w=700&q=55&auto=format&usm=12&fit=max&s=4f322feb316b7d8734e0876371880b75",
+      "scores": [5,2,5,3,5,5,1,5,5,3]
     },
+
+    {
+      "name": "Virginia Woolf",
+      "photo": "https://media.newyorker.com/photos/59095ecb6552fa0be682df8b/4:3/w_960,c_limit/Matar-Virginia-Woolf.jpg",
+      "scores": [5,2,5,1,4,1,1,1,4,1]
+    },
+
+    {
+      "name": "Oscar Wilde",
+      "photo": "http://www.outatstpaul.org/wp-content/uploads/2015/06/oscar_3.jpg",
+      "scores": [1,5,5,2,5,5,1,1,3,5]
+    },
+
+    {
+      "name": "Leo Tolstoy",
+      "photo": "http://cdn.history.com/sites/2/2016/01/GettyImages-463960391-E.jpeg",
+      "scores": [2,1,3,5,4,5,1,2,4,5]
+    },
+
+    {
+      "name": "Margaret Atwood",
+      "photo": "https://media.npr.org/assets/img/2014/09/10/margaretatwood-creditjeanmalek-_wide-07a2083c0b43b7b4c4a1bf18473648fd56af5763.jpg?s=1400",
+      "scores": [5,2,5,1,4,1,1,4,2,2]
+    },
+
+    {
+      "name": "David Foster Wallace",
+      "photo": "https://i1.wp.com/www.brainpickings.org/wp-content/uploads/2015/11/dfw5.jpg?fit=600%2C315&ssl=1",
+      "scores": [5,5,1,3,5,3,3,3,5,5]
+    },
+
+        
 ]
 
 module.exports = friendArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,89 @@ module.exports = function (app) {
 
 //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post('/api/friends', function(req, res){
+    //    //compare incoming scores with all other scores
+    //    var userData = req.body;
+    //    var difference = [];
+     //
+    //    //if more than one obj in friends.js
+    //    if(friendData.length >1) {
+    //      friendData.forEach(function(user){
+    //        var totalDiff = 0;
+     //
+    //        for(var i = 0; i<userData.scores.length; i++){
+    //          var dbScore = user.scores[i];
+    //          var userScore = userData.scores[i];
+    //          var diff = +dbScore - +userScore;
+    //          totalDiff += Math.abs(diff);
+    //        }
+     //
+    //        difference.push(totalDiff);
+    //      });
+     //
+    //      var minDiff = Math.min.apply(null, difference);
+     //
+    //      var bestMatch = [];
+     //
+    //      for (var i=0; i < difference.length; i++){
+    //        if(difference[i] === minDiff){
+    //          bestMatch.push(friendData[i]);
+    //        }
+    //      }
+     //
+    //      res.json(bestMatch);
+    //    } else {
+    //      res.json(friendData);
+    //    }
+     //
+    //  friendData.push(userData);
+
+
+
+//FIRST ATTEMPT
+       //get the data points from the incoming req
+       var userData = req.body;
+       var userName = userData.name;
+       var userPhoto = userData.photo;
+       var userScores= userData.scores;
+
+       console.log(userData);
+       //var for comparing difference
+       var difference = 0;
+       //var obj to find best match
+       var bestFriend = {
+         name: "",
+         photo: "",
+         setDiff: 55
+       };
+
+       //loop through the friends in friends.js
+       for(var i=0; i<friendData.length; i++){
+         //loop through scores of each friend and compare to scores of
+         //incoming req
+         console.log(friendData[i].name);
+          difference = 0;
+
+         for(var j = 0; j< friendData[i].scores[j]; i++){
+           //add together the absolute values of the differences between each //array location
+
+           difference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+
+           console.log(difference);
+
+           if(difference <= bestFriend.setDiff){
+             bestFriend.name = friendData[i].name;
+             bestFriend.photo = friendData[i].photo;
+             bestFriend.setDiff = difference;
+           }
+         }
+       }
+
+
+
+    //push data to friends.js AFTER finding best match, else will match with self
     friendData.push(req.body);
-    res.json(true);
+
+    //best match in json format
+    res.json(bestFriend);
   });
 }
